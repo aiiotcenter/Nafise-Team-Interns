2266f37a01f97f87c88257ef956ce1c15c43a3c3
HasanShkoukani <hasanshkoukani19@gmail.com>
2025-03-07 01:04:35 +0200

    loading interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,55 @@
 "use client";
 import React, { useState } from "react";
 import { Button } from "./components/Button/Button";
-import Calendar from "./components/Calendar/Calendar"
-
+import Calendar from "./components/Calendar/Calendar";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 const Home: React.FC = () => {
-  const [display, setdisplay] = useState(true);
+  const [display, setDisplay] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleClick = () => {
-    setdisplay(true);
+  const handleTimeout = () => {
+    setLoading(false);
+    setDisplay(true);
   };
 
-
-  // @Hassan & @Amir
-  // here you can see an example of how to use a component 
-  // your components must be flexable as well , 
-  // for example this button can take any color and text and onClick function so we can use it anywhere in our code
   return (
-    <div style={{ textAlign: "center", marginTop: "50px"}}>
+    <div style={{ textAlign: "center", marginTop: "50px", alignContent: "center" }}>
       <h1>Home Page</h1>
       <Button
-        onClick={handleClick}
-        place=''
+        onClick={() => {
+          setLoading(true);
+          setTimeout(handleTimeout, 3000);
+        }}
+        place=""
         color="gray"
       >
         Click On Me
       </Button>
-      {
-      display && 
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: "15px"}}>
-          <Calendar
+
+      {loading ? (
+        <div
+          style={{
+            height: "70vh",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <PropagateLoader className="loading" color="#98c2e0" size={15} />
+        </div>
+      ) : (
+        display && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "15px",
+            }}
+          >
+            <Calendar
               events={[
                 {
                   start: new Date(2025, 0, 28, 12, 45),
@@ -42,11 +62,12 @@ const Home: React.FC = () => {
                   end: new Date(2025, 0, 29, 9, 30),
                   title: "Yoga session",
                   color: "green",
-                },  
-              ]}              
-          />
-        </div>
-      }
+                },
+              ]}
+            />
+          </div>
+        )
+      )}
     </div>
   );
 };
